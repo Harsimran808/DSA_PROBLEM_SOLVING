2f2286f2338e68da52d1738ad6d20e18b37d094e
Harsimran808 <harsimransingh6234@gmail.com>
2024-06-17 13:34:55 +0530

    Update PREFIX_TO_INFIX.c

--- PREFIX_TO_INFIX.c
+++ PREFIX_TO_INFIX.c
@@ -1,56 +1,35 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
-int top=-1;
-#define MAX 100
-
-void push(char stack[], char c) {
-    if (top == MAX - 1) {
-        printf("Overflow\n");
-        exit(1);
-    }
-    stack[++top] = c;
-}
-
-char pop(char stack[]) {
-    if (top == -1) {
-        printf("Underflow\n");
-        exit(1);
-    }
-    return stack[top--];
-}
-
-void prefixtoinfix(char prefix[]) {
-    int i, len;
-    char stack[MAX];
-    char temp[MAX];
-    
-
-    len = strlen(prefix);
-    for (i = len - 1; i >= 0; i--) {
-        if (isalnum(prefix[i])) {
-            push(stack, prefix[i]);
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+#define max 100
+void prefixtoinfix(char source[], char target[]) {
+    int i=0; 
+    int k=0;
+    int len=strlen(source);
+    char temp[100], s1[100][30];
+    strcpy(target, "");
+    for(i=len-1;i>=0;i--) {
+        if (isalnum(source[i])) {
+            char t[]={source[i],'\0'};
+            strcpy(s1[k++], t);
+            
         } else {
-            char a = pop(stack);
-            char b = pop(stack);
-            sprintf(temp, "(%c %c %c)", a, prefix[i], b);
-            int j=0;
-            while(temp[j]!='\0')
-            {
-                push(stack,temp[j]);
-                j++;
-            }
+            char *op1 = s1[--k]; 
+            char *op2 = s1[--k]; 
+            sprintf(temp, "(%s%c%s)", op1, source[i], op2);
+            strcpy(s1[k++], temp); 
+            
         }
-    }
-
-    printf("Infix expression: %s\n", stack);
+    } 
+    strcpy(target, s1[--k]); 
 }
-
-int main() {
-    char prefix[MAX];
-    printf("Enter the prefix expression: ");
-    scanf("%s", prefix);
-    prefixtoinfix(prefix);
-    return 0;
-}
+int main()
+{
+    char prefix[max];
+    printf("Enter the prefix expression\n");
+    scanf("%s",prefix);
+    char infix[max];
+    prefixtoinfix(prefix,infix);
+    printf("Infix expression is %s\n",infix);
+}*/
